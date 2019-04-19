e90b4bc641e905787a55a2f6abd8ecd39f1c9d4c
yongli <yongli@qq.com>
2019-04-19 14:47:37 +0800

    新增导出功能

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
  * @Author: Leo 
  * @Date: 2019-04-15 10:37:36 
  * @Last Modified by: Leo
- * @Last Modified time: 2019-04-16 14:37:23
+ * @Last Modified time: 2019-04-19 13:52:56
  */
 
 'use strict';
@@ -35,9 +35,12 @@ router.get('/send', function(req, res, next) {
         fromTel: config.fromTel
       }
     });  
-  }
-  
+  }  
 });
+
+// router.get('/show', function(req, res, next) {  
+//   res.render(`show`);  
+// });
 
 router.post('/login', function(req, res, next) {
   let txtAccount = req.body.txtAccount;
@@ -56,56 +59,122 @@ router.post('/login', function(req, res, next) {
 router.post('/show', function(req, res, next) {
   let msgBody = req.body.txtBody;
   let msgTo = req.body.txtTo;
+  let msgArea = req.body.txtArea;
 
-  if(!msgBody || !msgTo){
+  if(!msgBody || !msgTo || !msgArea){
     return;
   }
   
-  
+  //将换行符替换成逗号
+  msgTo = msgTo.replace(/\r\n/g, ',');  
   let toArr = msgTo.split(',');
   let successList = [];
   let failedList = [];
   let validTo = [];
+  let telephone = '';
   for (let i = 0; i < toArr.length; i++){
-    if (toArr[i] && toArr[i].length > 5){
-      validTo.push(toArr[i].replace(/\r\n/g, ''));
+    if (toArr[i]){
+      if (toArr[i].length > 5){
+        telephone = toArr[i];
+        telephone = msgArea + telephone;
+        validTo.push(telephone);
+      } else {
+        failedList.push({
+          failed: msgArea + toArr[i]
+        });
+      }      
     }
   }
 
-  co(function *() {
+  // for (let i = 0; i < validTo.length; i++){
+  //   co(function *() {
+  //     yield client.messages.create({  
+  //       from: config.fromTel,       
+  //       to: validTo[i],
+  //       body: msgBody 
+  //     }, function(err, result) {
+  //       if (!err){          
+  //         console.log(result.sid);
+  //         successList.push(validTo[i]);
+  //       } else {
+  //         console.log('send failed: ' + err.message);
+  //         failedList.push(validTo[i]);          
+  //       }        
+  //     })
+  //   }).catch(function (err) {
+  //     console.log('catch error: ' + err.message);    
+  //   });  
+  // }
+  // res.render(`show`, {
+  //   data: {
+  //     failed: failedList,
+  //     success: successList
+  //   }
+  // });  
+  
+
+  // co(function *() {
+  //   for (let i = 0; i < validTo.length; i++){
+  //     yield client.messages.create({  
+  //         from: config.fromTel,       
+  //         to: validTo[i],
+  //         body: msgBody 
+  //       }, function(err, result) {
+  //         if (!err){          
+  //           console.log(result.sid);
+  //           successList.push(validTo[i]);
+  //         } else {
+  //           console.log('catch error: ' + err.message);    
+  //           failedList.push(validTo[i]);          
+  //         }        
+  //     })       
+  //   }
+  // }).catch(function (err) {
+  //   console.log('catch error: ' + err.message);    
+  // }); 
+  // res.render(`show`, {
+  //   data: {
+  //     failed: failedList,
+  //     success: successList
+  //   }
+  // }); 
+
+  co(function* () {
+    let sendPromises = [];
     for (let i = 0; i < validTo.length; i++){
-      yield client.messages.create({  
+      let promise = co(function *() {
+        yield client.messages.create({  
           from: config.fromTel,       
           to: validTo[i],
           body: msgBody 
         }, function(err, result) {
           if (!err){          
             console.log(result.sid);
-            successList.push(validTo[i]);
+            successList.push({
+              success: validTo[i]
+            });
           } else {
-            failedList.push(validTo[i]);          
+            console.log('send failed: ' + err.message);    
+            failedList.push({
+              failed: validTo[i]
+            });          
           }        
-      })       
+        })
+      }).catch(function (err) {
+        
+      });
+      sendPromises.push(promise);      
     }
+
+    yield sendPromises;
 
     res.render(`show`, {
       data: {
-        failed: failedList,
-        success: successList
+        failedList: failedList,
+        successList: successList
       }
-    });    
-  })
-  .catch(function (err) {
-    // console.log(err.message);
-    res.render(`show`, {
-      data: {
-        failed: failedList,
-        success: successList
-      }
-    });    
-  }); 
-  
-  
+    });
+  });   
 });
 
 module.exports = router;
